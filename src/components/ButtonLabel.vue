ec595f512967d39c34756bf5f8d46da76f1921d8
Don Michael Gavieta <michaelgavieta@gmail.com>
2024-06-04 09:07:58 +0800

    a

---
+++ src/components/ButtonLabel.vue
@@ -0,0 +1,24 @@
+<template>
+    <button type="button"  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" :disabled="disabled">
+        <span class="text-white"> {{ label }} </span> 
+    </button>
+</template>
+
+<script>
+export default {
+    name: "ButtonLabel",
+    props: {
+        label: {
+            type: String,
+            default: ''
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+    },
+    computed: {
+ 
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './style.css' 
+import router from './router'
+import App from './App.vue' 
+import { initializeApp } from "firebase/app"; 
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyBfPUouEWQFd0XXF_6ol0iLR_NCyuFm0QA",
+  authDomain: "realtime-chat-proj.firebaseapp.com",
+  projectId: "realtime-chat-proj",
+  storageBucket: "realtime-chat-proj.appspot.com",
+  messagingSenderId: "1047707809283",
+  appId: "1:1047707809283:web:c9e47f0cf10b2aba7fc036"
+};
 
-createApp(App).mount('#app')
+initializeApp(firebaseConfig); 
+
+createApp(App).use(router).mount('#app')
